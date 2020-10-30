639711864f85e737d424a4d27de1b5615fa11fdb
Oleg Kruchay <okruchay@gmail.com>
2020-10-30 11:24:23 +0200

    v0.3: Configuring the Global page headers. Importing the favicons settings. Updating the nuxt.config.js file.

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,89 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hub-olegkruchay-com',
+    title:
+      "It's a Hub | The list of projects of olegkruchay.com | Front-end developer 👨🏻‍💻",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+
+      // Reference for Drupal
+      { name: 'generator', content: 'Drupal 10 (http://drupal.org)' },
+      {
+        hid: 'description',
+        name: 'description',
+        // eslint-disable-next-line max-len
+        content:
+          'Front-end web developer (React, Vue, Drupal 7-9). Based in Kyiv, Ukraine. Social: @olegkruchay. Mail: okruchay@gmail.com',
+      },
+      {
+        property: 'og:title',
+        content:
+          "It's a Hub | The list of projects of olegkruchay.com | Front-end developer 👨🏻‍💻",
+      },
+      {
+        property: 'og:description',
+        // eslint-disable-next-line max-len
+        content:
+          'Front-end web developer (React, Vue, Drupal 7-9). Based in Kyiv, Ukraine. Social: @olegkruchay. Mail: okruchay@gmail.com',
+      },
+      {
+        property: 'og:url',
+        content: 'http://hub.olegkruchay.com',
+      },
+      {
+        property: 'og:url:secure_url',
+        content: 'https://hub.olegkruchay.com',
+      },
+      {
+        property: 'og:site_name',
+        content:
+          "It's a Hub | The list of projects of olegkruchay.com | Front-end developer 👨🏻‍💻",
+      },
+      {
+        property: 'og:image',
+        content: 'https://hub.olegkruchay.com/public/oleg.png',
+      },
+      {
+        property: 'og:image:type',
+        content: 'image/png',
+      },
+      {
+        property: 'og:image:alt',
+        content: 'Oleg Kruchay portrait',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        type: 'image/png',
+        href: '/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png',
+        href: '/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: '/favicons/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/favicons/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/favicons/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@/assets/css/styles.scss'
-  ],
+  css: ['@/assets/css/styles.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -38,35 +108,33 @@ export default {
       '@nuxtjs/fontawesome',
       {
         component: 'fa',
-      icons: {
-        regular: [
-          'faHeart',
-          'faEnvelope'
-        ],
-        brands: [
-          'faInstagram',
-          'faFacebook',
-          'faFacebookMessenger',
-          'faCodepen',
-          'faDrupal',
-          'faLinkedin',
-          'faTelegram',
-          'faSkype',
-          'faGithub',
-          'faJs',
-          'faNodeJs',
-          'faReact',
-          'faVuejs',
-          'faDocker',
-          'faSymfony',
-          'faPhp',
-          'faPiedPiperPp',
-          'faCreativeCommons',
-          'faCreativeCommonsBy',
-          'faCreativeCommonsSa',
-        ]
-      }
-    }]
+        icons: {
+          regular: ['faHeart', 'faEnvelope'],
+          brands: [
+            'faInstagram',
+            'faFacebook',
+            'faFacebookMessenger',
+            'faCodepen',
+            'faDrupal',
+            'faLinkedin',
+            'faTelegram',
+            'faSkype',
+            'faGithub',
+            'faJs',
+            'faNodeJs',
+            'faReact',
+            'faVuejs',
+            'faDocker',
+            'faSymfony',
+            'faPhp',
+            'faPiedPiperPp',
+            'faCreativeCommons',
+            'faCreativeCommonsBy',
+            'faCreativeCommonsSa',
+          ],
+        },
+      },
+    ],
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -74,24 +142,25 @@ export default {
     [
       '@nuxtjs/google-fonts',
       {
-      families: {
-        Niconne: true,
-        Righteous: true,
-        'Patua+One': true,
-        Comfortaa: {
-          wght: [300, 400, 700],
+        families: {
+          Niconne: true,
+          Righteous: true,
+          'Patua+One': true,
+          Comfortaa: {
+            wght: [300, 400, 700],
+          },
         },
+        display: 'swap',
+        download: true,
       },
-      display: 'swap',
-      download: true,
-    }],
-    '@nuxtjs/svg'
+    ],
+    '@nuxtjs/svg',
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
-  
+
   googleAnalytics: {
     // id: 'UA-131497403-1'
-  }
+  },
 }
