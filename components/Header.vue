b4925d78ee049fa84ea4a30356ad86a1acad549c
Oleg Kruchay <okruchay@gmail.com>
2020-10-30 03:08:28 +0200

    v0.2: Importing the following components: (Header, Footer, Main, AnimatedGrid) from [olegkruchay-vue] repo.

---
+++ components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="ok-header">
+    <!--  Column 1  -->
+    <div class="ok-column first">
+      <NuxtLink
+        exact-active-class="ok-link active"
+        class="ok-link ok-link-header"
+        :to="header.contactsLink.contactsLinkUri"
+      >
+        <span class="ok-hide mobile">{{
+          header.contactsLink.contactsLinkTitle
+        }}</span>
+        <span class="ok-show mobile">{{ '📨' }}</span>
+      </NuxtLink>
+    </div>
+
+    <!--  Column 2  -->
+    <div class="ok-column second">
+      <NuxtLink to="/" class="ok-link logo">
+        <HeaderLogo />
+      </NuxtLink>
+    </div>
+
+    <!--  Column 3  -->
+    <div class="ok-column third">
+      <NuxtLink
+        exact-active-class="ok-link active"
+        class="ok-link ok-link-header"
+        :to="header.aboutLink.aboutLinkUri"
+      >
+        <span class="ok-hide mobile">{{
+          header.aboutLink.aboutLinkTitle
+        }}</span>
+        <span class="ok-show mobile">{{ 'i' }}</span>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
+
+<script>
+import { header } from '~/static/data/data.json'
+import HeaderLogo from '~/assets/images/logo.svg?inline'
+
+export default {
+  name: 'Header',
+  components: { HeaderLogo },
+  data() {
+    return {
+      header,
+    }
+  },
+}
+</script>
